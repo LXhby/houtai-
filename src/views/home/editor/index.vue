94727dfabd83402a9e3c15df4945e965abb3cada
胡冰玉 <hubingyu@hubingyuiMac.local>
2019-06-06 16:51:02 +0800

    后台

---
+++ src/views/home/editor/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>editor首页</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeEditor',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
